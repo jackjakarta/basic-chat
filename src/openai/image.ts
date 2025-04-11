0fc4775dd6700c7b3ab4c20bb6cb34ddc8a9d62d
jackjakarta <al.termure@gmail.com>
2025-04-12 01:23:03 +0200

    feat: update image generation

--- src/openai/image.ts
+++ src/openai/image.ts
@@ -1,10 +1,29 @@
 import { openai } from '.';
 
-export async function generateImage({ prompt }: { prompt: string }) {
+type ImageQuality = 'standard' | 'hd';
+type ImageStyle = 'natural' | 'vivid';
+type ImageSize = '1024x1792' | '1792x1024' | '1024x1024';
+
+export async function generateImage({
+  prompt,
+  quality = 'standard',
+  amount = 1,
+  style = 'vivid',
+  size = '1792x1024',
+}: {
+  prompt: string;
+  quality?: ImageQuality;
+  amount?: number;
+  style?: ImageStyle;
+  size?: ImageSize;
+}) {
   const image = await openai.images.generate({
     model: 'dall-e-3',
     prompt,
-    n: 1,
+    n: amount,
+    quality,
+    size,
+    style,
   });
 
   const imageUrl = image.data[0]?.url;
