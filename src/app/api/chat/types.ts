0befd1a7b050e21f700f869294935755de558c08
jackjakarta <al.termure@gmail.com>
2025-04-16 23:26:01 +0200

    feat: add gpt-4.1 models

--- src/app/api/chat/types.ts
+++ src/app/api/chat/types.ts
@@ -1,5 +1,11 @@
 import { z } from 'zod';
 
-export const modelsSchema = z.enum(['gpt-4o-mini', 'gpt-4o', 'gpt-4.5-preview']);
+export const modelsSchema = z.enum([
+  'gpt-4.1',
+  'gpt-4.1-mini',
+  'gpt-4.1-nano',
+  'gpt-4o',
+  'gpt-4o-mini',
+]);
 
 export type AIModel = z.infer<typeof modelsSchema>;
