80ec1a7e31ec6f88bfe15c34ced737a1371f96a0
jackjakarta <al.termure@gmail.com>
2025-06-06 20:59:37 +0200

    feat: squash migrations

--- src/db/migrations/0019_cheerful_proudstar.sql
+++ src/db/migrations/0019_cheerful_proudstar.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS "app"."assistant" (
 	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+ALTER TABLE "app"."conversation" ADD COLUMN "assistant_id" uuid;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "app"."assistant" ADD CONSTRAINT "assistant_user_id_user_entity_id_fk" FOREIGN KEY ("user_id") REFERENCES "app"."user_entity"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
@@ -17,6 +18,12 @@ END $$;
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "app"."assistant" ADD CONSTRAINT "assistant_vector_store_id_vector_store_id_fk" FOREIGN KEY ("vector_store_id") REFERENCES "app"."vector_store"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "app"."conversation" ADD CONSTRAINT "conversation_assistant_id_assistant_id_fk" FOREIGN KEY ("assistant_id") REFERENCES "app"."assistant"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
