ef4eb0ee3919ed4ad233464dd967d5247b5b7cc5
jackjakarta <al.termure@gmail.com>
2025-06-07 23:26:30 +0200

    feat: chat ui improvements

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -36,88 +36,89 @@ export default function ChatMessages({ messages, status, reload }: ChatMessagesP
           message.experimental_attachments?.filter((a) => a.type === 'file') ?? [];
 
         return (
-          <div
-            key={index}
-            className={cw(
-              'w-fit text-secondary-foreground',
-              message.role === 'user' &&
-                'p-4 rounded-2xl rounded-br-none self-end bg-sidebar-accent text-secondary-foreground max-w-[70%] break-words',
-            )}
-          >
-            <div>
-              {message.content.length > 0 && status !== 'error' ? (
-                <>
-                  {userFileAttachments.length > 0 && (
-                    <div className="flex items-center flex-wrap gap-x-2">
-                      {userFileAttachments.map((attachment) => (
-                        <DisplayPdfFile
-                          key={attachment.id}
-                          fileName={extractFileNameFromSignedUrl(attachment.name)}
-                          className="w-[100px] bg-sidebar mb-4"
-                        />
-                      ))}
-                    </div>
-                  )}
-
-                  {userImageAttachments.length > 0 && (
-                    <div className="flex flex-col gap-2">
-                      {userImageAttachments.map((attachment) => (
-                        <Image
-                          key={attachment.id}
-                          src={attachment.url}
-                          alt={`logo-${attachment.id}`}
-                          width={300}
-                          height={300}
-                          className="rounded-lg h-[300px] w-[300px] object-cover mb-4"
-                        />
-                      ))}
-                    </div>
-                  )}
-
-                  <MarkdownDisplay maxWidth={700}>{message.content}</MarkdownDisplay>
-                  <DisplayCodeExecution message={message} status={status} />
-                  <DisplaySources message={message} status={status} />
-                </>
-              ) : (
-                <LoadingTool message={message} />
-              )}
-
-              {finishedAssistantMessage && (
-                <div
-                  className={cw(
-                    'flex items-center gap-1 hover:opacity-100',
-                    isLastNonUser ? 'opacity-100' : 'opacity-0',
-                  )}
-                >
-                  <CopyButton text={message.content} index={index} />
-                  <TTSButton
-                    text={message.content}
-                    className={cw(
-                      'mt-1 p-2 rounded-md hover:bg-secondary/65',
-                      'text-primary dark:text-sidebar-accent',
-                    )}
-                    iconClassName="w-3.5 h-3.5"
+          <div key={index} className="flex flex-col  gap-2">
+            {userFileAttachments.length > 0 && (
+              <div className="flex items-center flex-wrap gap-x-2 self-end">
+                {userFileAttachments.map((attachment) => (
+                  <DisplayPdfFile
+                    key={attachment.id}
+                    fileName={extractFileNameFromSignedUrl(attachment.name)}
+                    className="w-[100px] bg-sidebar"
                   />
-                  {isLastNonUser && (
-                    <button
-                      title="Reload last message"
-                      type="button"
-                      onClick={() => reload()}
-                      className="mt-1"
-                      aria-label="Reload"
-                    >
-                      <div
-                        className={cw(
-                          'p-2 rounded-md hover:bg-secondary/65',
-                          'text-primary dark:text-sidebar-accent',
-                        )}
-                      >
-                        <ReloadIcon className="w-3.5 h-3.5" />
-                      </div>
-                    </button>
-                  )}
-                </div>
+                ))}
+              </div>
+            )}
+
+            {userImageAttachments.length > 0 && (
+              <div className="flex flex-col gap-2 self-end">
+                {userImageAttachments.map((attachment) => (
+                  <Image
+                    key={attachment.id}
+                    src={attachment.url}
+                    alt={`logo-${attachment.id}`}
+                    width={300}
+                    height={300}
+                    className="rounded-lg h-[300px] w-[300px] object-cover bg-sidebar"
+                  />
+                ))}
+              </div>
+            )}
+
+            <div
+              className={cw(
+                'w-fit text-secondary-foreground',
+                message.role === 'user' &&
+                  'p-4 rounded-2xl rounded-br-none self-end bg-sidebar-accent text-secondary-foreground max-w-[70%] break-words',
               )}
+            >
+              <div>
+                {message.content.length > 0 && status !== 'error' ? (
+                  <>
+                    <MarkdownDisplay maxWidth={700}>{message.content}</MarkdownDisplay>
+                    <DisplayCodeExecution message={message} status={status} />
+                    <DisplaySources message={message} status={status} />
+                  </>
+                ) : (
+                  <LoadingTool message={message} />
+                )}
+
+                {finishedAssistantMessage && (
+                  <div
+                    className={cw(
+                      'flex items-center gap-1 hover:opacity-100',
+                      isLastNonUser ? 'opacity-100' : 'opacity-0',
+                    )}
+                  >
+                    <CopyButton text={message.content} index={index} />
+                    <TTSButton
+                      text={message.content}
+                      className={cw(
+                        'mt-1 p-2 rounded-md hover:bg-secondary/65',
+                        'text-primary dark:text-sidebar-accent',
+                      )}
+                      iconClassName="w-3.5 h-3.5"
+                    />
+                    {isLastNonUser && (
+                      <button
+                        title="Reload last message"
+                        type="button"
+                        onClick={() => reload()}
+                        className="mt-1"
+                        aria-label="Reload"
+                      >
+                        <div
+                          className={cw(
+                            'p-2 rounded-md hover:bg-secondary/65',
+                            'text-primary dark:text-sidebar-accent',
+                          )}
+                        >
+                          <ReloadIcon className="w-3.5 h-3.5" />
+                        </div>
+                      </button>
+                    )}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         );
