a485dbcc33288f4a19f55b32426566ec97f9b355
jackjakarta <al.termure@gmail.com>
2025-06-28 22:33:47 +0200

    feat: update hooks for dark mode

--- src/app/admin/users/actions.ts
+++ src/app/admin/users/actions.ts
@@ -1,14 +1,9 @@
 'use server';
 
 import { dbDeleteUser } from '@/db/functions/user';
-import { getUser } from '@/utils/auth';
+import { getSuperAdmin } from '@/utils/auth';
 
 export async function deleteUserAction({ userId }: { userId: string }) {
-  const user = await getUser();
-
-  if (!user.isSuperAdmin) {
-    throw new Error('Unauthorized: Only super admins can delete users.');
-  }
-
+  await getSuperAdmin();
   await dbDeleteUser({ userId });
 }
