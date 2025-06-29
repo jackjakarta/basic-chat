1a1750eaaaeecf148fb2aca25fcfd7e492efb7b1
Alex Termure <124639246+jackjakarta@users.noreply.github.com>
2025-06-29 16:39:24 +0200

    refactor: password validator logic (#112)

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,8 +1,8 @@
-import { passwordValidatorSchema } from '@/env';
 import { z } from 'zod';
 
 import { siteLanguageSchema } from './schemas';
 
 export type SVGProps = React.ComponentProps<'svg'>;
-export type PasswordValidatorLevel = z.infer<typeof passwordValidatorSchema>;
 export type SiteLanguage = z.infer<typeof siteLanguageSchema>;
+
+export type PasswordValidatorLevel = 'weak' | 'medium' | 'strong';
