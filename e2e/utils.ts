68c52d5d84921a4853f9e76c9578656644339995
jackjakarta <al.termure@gmail.com>
2025-09-27 18:22:47 +0200

    cicd: improve workflows

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,10 +1,12 @@
 export const MOCK_USER = (() => {
   const email = process.env.E2E_USER_EMAIL;
   const password = process.env.E2E_USER_PASSWORD;
+
   if (!email || !password) {
     throw new Error(
       'E2E test environment variables E2E_USER_EMAIL and E2E_USER_PASSWORD must be set.',
     );
   }
+
   return { email, password };
 })();
